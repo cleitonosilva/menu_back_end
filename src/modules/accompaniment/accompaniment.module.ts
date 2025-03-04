b08fb071ca4549c06a0aeac3e571c4b13925ff60
Cleiton Oliveira <cleiton.o.dev@gmail.com>
2025-03-04 22:52:48 +0000

    :art: atualiza o módulo de acompanhamento para incluir novos schemas e validações de alimentos

--- src/modules/accompaniment/accompaniment.module.ts
+++ src/modules/accompaniment/accompaniment.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Accompaniment, AccompanimentSchema } from '../../schemas/accompaniment.schema';
-import { FoodModule } from '../food/food.module';
-import { AccompanimentController } from './controllers/accompaniment.controller';
+import { Food, FoodSchema } from '../../schemas/food.schema'; 
 import { AccompanimentService } from './services/accompaniment.service';
+import { AccompanimentController } from './controllers/accompaniment.controller';
+import { FoodModule } from '../food/food.module';
+import { Carbohydrate, CarbohydrateSchema } from 'src/schemas/carbohydrate.schema';
+import { Medicines, MedicinesSchema } from 'src/schemas/medicines.schema';
+import { Protein, ProteinSchema } from 'src/schemas/protein.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Accompaniment.name, schema: AccompanimentSchema }]),
-    FoodModule
+    MongooseModule.forFeature([
+        { name: Accompaniment.name, schema: AccompanimentSchema },
+        { name: Food.name, schema: FoodSchema },
+        { name: Carbohydrate.name, schema: CarbohydrateSchema },
+        { name: Protein.name, schema: ProteinSchema },
+        { name: Medicines.name, schema: MedicinesSchema },
+    ]),
+    FoodModule 
   ],
   controllers: [AccompanimentController],
   providers: [AccompanimentService],
--- src/modules/accompaniment/services/accompaniment.service.ts
+++ src/modules/accompaniment/services/accompaniment.service.ts
@@ -1,23 +1,72 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Accompaniment } from '../../../schemas/accompaniment.schema';
+import { Food } from '../../../schemas/food.schema'; 
 import { CreateAccompanimentDto } from '../dto/create-accompaniment.dto';
+import { isFoodAlreadyUsed } from 'src/common/utils/food-validation.util';
+import { Carbohydrate } from 'src/schemas/carbohydrate.schema';
+import { Medicines } from 'src/schemas/medicines.schema';
+import { Protein } from 'src/schemas/protein.schema';
 
 @Injectable()
 export class AccompanimentService {
-  constructor(@InjectModel(Accompaniment.name) private accompanimentModel: Model<Accompaniment>) {}
+  constructor(
+    @InjectModel(Carbohydrate.name) private carbohydrateModel: Model<Carbohydrate>,
+    @InjectModel(Protein.name) private proteinModel: Model<Protein>, 
+    @InjectModel(Accompaniment.name) private accompanimentModel: Model<Accompaniment>,
+    @InjectModel(Medicines.name) private medicinesModel: Model<Medicines>, 
+    @InjectModel(Food.name) private foodModel: Model<Food> 
+    
+  ) {}
 
   async create(data: CreateAccompanimentDto): Promise<Accompaniment> {
-    return this.accompanimentModel.create(data);
-  }
+    const foodExists = await this.foodModel.findById(data.food);
+    if (!foodExists) {
+      throw new NotFoundException('O alimento fornecido não existe.');
+    }
 
+    const foodUsed = await isFoodAlreadyUsed(
+      data.food,
+      this.carbohydrateModel,
+      this.proteinModel,
+      this.accompanimentModel,
+      this.medicinesModel
+    );
+    if (foodUsed) {
+      throw new BadRequestException('O alimento já pertence a outra categoria.');
+    }
+  
+    const accompaniment = await this.accompanimentModel.create({
+      food: foodExists._id, 
+      grams: data.grams,
+      tablespoon: data.tablespoon
+    });
+  
+    return accompaniment.populate('food'); 
+  }
+  
   async findAll(): Promise<Accompaniment[]> {
     return this.accompanimentModel.find().populate('food', 'name').exec();
   }
 
   async update(id: string, data: Partial<CreateAccompanimentDto>): Promise<Accompaniment | null> {
-    return this.accompanimentModel.findByIdAndUpdate(id, data, { new: true }).exec();
+    if (data.food) {
+      const foodExists = await this.foodModel.findById(data.food);
+      if (!foodExists) {
+        throw new NotFoundException('O alimento fornecido não existe.');
+      }
+    }
+
+    const updatedAccompaniment = await this.accompanimentModel
+      .findByIdAndUpdate(id, data, { new: true })
+      .populate('food'); 
+
+    if (!updatedAccompaniment) {
+      throw new NotFoundException('Acompanhamento não encontrado.');
+    }
+
+    return updatedAccompaniment;
   }
 
   async delete(id: string): Promise<Accompaniment | null> {
