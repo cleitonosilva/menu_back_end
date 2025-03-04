301390bf6edc5a441c43f72e5e8607d735c68047
Cleiton Oliveira <cleiton.o.dev@gmail.com>
2025-03-04 23:09:04 +0000

    :art: atualiza o módulo de alimentos para incluir novos schemas e serviços de acompanhamento, carboidrato e proteína, e aprimora a função de busca para retornar categorias e detalhes adicionais

--- src/modules/food/food.module.ts
+++ src/modules/food/food.module.ts
@@ -3,9 +3,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Food, FoodSchema } from '../../schemas/food.schema';
 import { FoodController } from './controllers/food.controller';
 import { FoodService } from './services/food.service';
+import { Accompaniment, AccompanimentSchema } from 'src/schemas/accompaniment.schema';
+import { Carbohydrate, CarbohydrateSchema } from 'src/schemas/carbohydrate.schema';
+import { Protein, ProteinSchema } from 'src/schemas/protein.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Food.name, schema: FoodSchema }])],
+  imports: [MongooseModule.forFeature([
+    { name: Food.name, schema: FoodSchema },
+    { name: Carbohydrate.name, schema: CarbohydrateSchema },
+    { name: Protein.name, schema: ProteinSchema },
+    { name: Accompaniment.name, schema: AccompanimentSchema },
+  ])],
   controllers: [FoodController],
   providers: [FoodService],
   exports: [FoodService, MongooseModule]
--- src/modules/food/services/food.service.ts
+++ src/modules/food/services/food.service.ts
@@ -3,10 +3,18 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Food } from '../../../schemas/food.schema';
 import { CreateFoodDto } from '../dto/create-food.dto';
+import { Accompaniment } from 'src/schemas/accompaniment.schema';
+import { Carbohydrate } from 'src/schemas/carbohydrate.schema';
+import { Protein } from 'src/schemas/protein.schema';
 
 @Injectable()
 export class FoodService {
-  constructor(@InjectModel(Food.name) private foodModel: Model<Food>) {}
+  constructor(
+    @InjectModel(Food.name) private foodModel: Model<Food>,
+    @InjectModel(Carbohydrate.name) private carbohydrateModel: Model<Carbohydrate>,
+    @InjectModel(Protein.name) private proteinModel: Model<Protein>,
+    @InjectModel(Accompaniment.name) private accompanimentModel: Model<Accompaniment>
+  ) {}
 
   async create(data: CreateFoodDto): Promise<Food> {
     const existingFood = await this.foodModel.findOne({ name: data.name }).exec();
@@ -16,9 +24,69 @@ export class FoodService {
     return this.foodModel.create(data);
   }
 
-  async findAll(): Promise<Food[]> {
-    return this.foodModel.find().exec();
+  async findAll(): Promise<Array<Omit<Food, keyof Document> & { category: string | null, categoryId: string | null, grams: number | null, tablespoon: number | null }>> {
+    const foods = await this.foodModel.find().lean(); 
+  
+    const foodWithCategory = await Promise.all(
+      foods.map(async (food) => {
+        const carbohydrate = await this.carbohydrateModel.findOne({ food: food._id }).lean();
+        const protein = await this.proteinModel.findOne({ food: food._id }).lean();
+        const accompaniment = await this.accompanimentModel.findOne({ food: food._id }).lean();
+  
+        let category: string | null = null;
+        let categoryId: string | null = null;
+        let grams: number | null = null;
+        let tablespoon: number | null = null;
+  
+        if (carbohydrate) {
+          category = 'Carbohydrate';
+          categoryId = carbohydrate._id.toString();
+          grams = carbohydrate.grams ?? null;
+          tablespoon = carbohydrate.tablespoon ?? null;
+        } else if (protein) {
+          category = 'Protein';
+          categoryId = protein._id.toString();
+          grams = protein.grams ?? null;
+          tablespoon = protein.tablespoon ?? null;
+        } else if (accompaniment) {
+          category = 'Accompaniment';
+          categoryId = accompaniment._id.toString();
+          grams = accompaniment.grams ?? null;
+          tablespoon = accompaniment.tablespoon ?? null;
+        }
+  
+        return {
+          ...food,
+          category, 
+          categoryId, 
+          grams, 
+          tablespoon 
+        };
+      })
+    );
+  
+    return foodWithCategory;
   }
+  
+
+  async findById(id: string): Promise<Omit<Food, keyof Document> & { category: string | null }> {
+    const food = await this.foodModel.findById(id).lean();
+    if (!food) {
+      throw new NotFoundException('Alimento não encontrado.');
+    }
+  
+    const isCarbohydrate = await this.carbohydrateModel.exists({ food: id });
+    const isProtein = await this.proteinModel.exists({ food: id });
+    const isAccompaniment = await this.accompanimentModel.exists({ food: id });
+  
+    let category: string | null = null;
+    if (isCarbohydrate) category = 'Carbohydrate';
+    if (isProtein) category = 'Protein';
+    if (isAccompaniment) category = 'Accompaniment';
+  
+    return { ...food, category };
+  }
+  
 
   async update(id: string, data: Partial<CreateFoodDto>): Promise<Food | null> {
     return this.foodModel.findByIdAndUpdate(id, data, { new: true }).exec();
