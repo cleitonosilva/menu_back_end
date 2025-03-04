a62936515cf319273eb456bbbdb45aa38724b16b
Cleiton Oliveira <cleiton.o.dev@gmail.com>
2025-03-04 14:48:18 +0000

    :art: adiciona o módulo MenuType ao AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MenuModule } from './modules/menu/menu.module';
+import { MenuTypeModule } from './modules/menu-types/menu-type.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/meu_banco'),
     MenuModule, 
+    MenuTypeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
