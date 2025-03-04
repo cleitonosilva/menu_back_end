df4c4e6154e7b0625564c0fd0de9d183c192dd34
Cleiton Oliveira <cleiton.o.dev@gmail.com>
2025-03-04 22:55:30 +0000

    :art: remove validação de medicamentos da função de verificação de alimentos em uso

--- src/common/utils/food-validation.util.ts
+++ src/common/utils/food-validation.util.ts
@@ -2,19 +2,16 @@ import { Model } from 'mongoose';
 import { Carbohydrate } from '../../schemas/carbohydrate.schema';
 import { Protein } from '../../schemas/protein.schema';
 import { Accompaniment } from '../../schemas/accompaniment.schema';
-import { Medicines } from '../../schemas/medicines.schema';
 
 export async function isFoodAlreadyUsed(
   foodId: string,
   carbohydrateModel: Model<Carbohydrate>,
   proteinModel: Model<Protein>,
-  accompanimentModel: Model<Accompaniment>,
-  medicinesModel: Model<Medicines>
+  accompanimentModel: Model<Accompaniment>
 ): Promise<boolean> {
   const existsInCarbohydrate = await carbohydrateModel.exists({ food: foodId });
   const existsInProtein = await proteinModel.exists({ food: foodId });
   const existsInAccompaniment = await accompanimentModel.exists({ food: foodId });
-  const existsInMedicines = await medicinesModel.exists({ food: foodId });
 
-  return !!(existsInCarbohydrate || existsInProtein || existsInAccompaniment || existsInMedicines);
+  return !!(existsInCarbohydrate || existsInProtein || existsInAccompaniment);
 }
