81349a4b7be690582e0ba96ae29d02ab64d5674a
Cleiton Oliveira <cleiton.o.dev@gmail.com>
2025-03-04 22:04:18 +0000

    4 de mar. de 2025, 22:04

--- src/modules/carbohydrate/services/carbohydrate.service.ts
+++ src/modules/carbohydrate/services/carbohydrate.service.ts
@@ -2,14 +2,26 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Carbohydrate } from '../../../schemas/carbohydrate.schema';
+import { Food } from '../../../schemas/food.schema';
 import { CreateCarbohydrateDto } from '../dto/create-carbohydrate.dto';
 
 @Injectable()
 export class CarbohydrateService {
-  constructor(@InjectModel(Carbohydrate.name) private carbohydrateModel: Model<Carbohydrate>) {}
+  constructor(
+    @InjectModel(Carbohydrate.name) private carbohydrateModel: Model<Carbohydrate>,
+    @InjectModel(Food.name) private foodModel: Model<Food>
+  ) {}
 
   async create(data: CreateCarbohydrateDto): Promise<Carbohydrate> {
-    return this.carbohydrateModel.create(data);
+    const foodExists = await this.foodModel.findById(data.food);
+    if (!foodExists) {
+      throw new NotFoundException('O alimento fornecido não existe.');
+    }
+
+    return this.carbohydrateModel.create({
+      ...data,
+      food: foodExists._id 
+    });
   }
 
   async findAll(): Promise<Carbohydrate[]> {
