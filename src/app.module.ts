5edfa420383eff40cf5c1653ee1c1cace2a40523
Cleiton Oliveira <cleiton.o.dev@gmail.com>
2025-03-04 13:48:13 +0000

    :art: adiciona configuração do Mongoose e do ConfigModule ao AppModule, removendo a configuração de banco de dados antiga

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { databaseProviders } from './config/database.config';
+import { MenuModule } from './modules/menu/menu.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/meu_banco'),
+    MenuModule, 
+  ],
   controllers: [AppController],
-  providers: [AppService, ...databaseProviders],
+  providers: [AppService],
 })
 export class AppModule {}
