731b9d4d1cb466a06c8d8ccc128559b9163e80be
Cleiton Oliveira <cleiton.o.dev@gmail.com>
2025-03-04 14:48:11 +0000

    :art: adiciona suporte a validação de dados com class-transformer e class-validator, configurando pipes globais no bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,32 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { BadRequestException, ValidationError, ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    exceptionFactory: (errors: ValidationError[]) => {
+      const formattedErrors = errors
+        .filter(err => err.constraints) 
+        .map(err => ({
+          field: err.property,
+          errors: Object.values(err.constraints || {}) 
+        }));
+
+      return new BadRequestException({
+        statusCode: 400,
+        error: 'Bad Request',
+        message: formattedErrors
+      });
+    }
+  }));
+
+  await app.listen(3000);
 }
 bootstrap();
