f817519dd1ed47c6989fbbcf25a0ddeba3a3f5b6
Cleiton Oliveira <cleiton.o.dev@gmail.com>
2025-03-04 21:48:31 +0000

    4 de mar. de 2025, 21:48

--- src/modules/food/controllers/food.controller.ts
+++ src/modules/food/controllers/food.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Get, Body } from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete, Body, Param, NotFoundException } from '@nestjs/common';
 import { FoodService } from '../services/food.service';
 import { CreateFoodDto } from '../dto/create-food.dto';
 
@@ -14,5 +14,23 @@ export class FoodController {
   @Get()
   async findAll() {
     return this.foodService.findAll();
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() data: Partial<CreateFoodDto>) {
+    const updatedFood = await this.foodService.update(id, data);
+    if (!updatedFood) {
+      throw new NotFoundException('Alimento não encontrado.');
+    }
+    return updatedFood;
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    const deleted = await this.foodService.delete(id);
+    if (!deleted) {
+      throw new NotFoundException('Alimento não encontrado.');
+    }
+    return { message: 'Alimento removido com sucesso' };
   }
 }
--- src/modules/food/services/food.service.ts
+++ src/modules/food/services/food.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import { Injectable, ConflictException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Food } from '../../../schemas/food.schema';
 import { CreateFoodDto } from '../dto/create-food.dto';
-import { Food } from 'src/schemas/food.schema';
 
 @Injectable()
 export class FoodService {
@@ -13,11 +13,18 @@ export class FoodService {
     if (existingFood) {
       throw new ConflictException('O alimento já existe.');
     }
-
     return this.foodModel.create(data);
   }
 
   async findAll(): Promise<Food[]> {
     return this.foodModel.find().exec();
+  }
+
+  async update(id: string, data: Partial<CreateFoodDto>): Promise<Food | null> {
+    return this.foodModel.findByIdAndUpdate(id, data, { new: true }).exec();
+  }
+
+  async delete(id: string): Promise<Food | null> {
+    return this.foodModel.findByIdAndDelete(id).exec();
   }
 }
